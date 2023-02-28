5c7131490f430911138c054bb89440f9dc7c19f0
Krishan Kumar <kkrishan7074@gmail.com>
2023-02-28 16:50:54 +0530

    Commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkLAielPBr4zJhjefhivVNp0vj6oVK3yc",
+    authDomain: "blog-app-7b725.firebaseapp.com",
+    projectId: "blog-app-7b725",
+    storageBucket: "blog-app-7b725.appspot.com",
+    messagingSenderId: "192172814511",
+    appId: "1:192172814511:web:680ab4820e35470ea1645a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
