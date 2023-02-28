0563712841e435e124e0327e630c62830bc232fb
Krishan Kumar <kkrishan7074@gmail.com>
2023-02-28 23:36:17 +0530

    Commit

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import { getDoc, doc } from 'firebase/firestore';
+import { useParams } from 'react-router-dom';
 
 function PostDetail() {
-  return <div>PostDetail</div>;
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    const postRef = doc(db, 'content', postId);
+
+    getDoc(postRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.data());
+        setPost(snapshot.data());
+      }
+    });
+  }, []);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
 }
 export default PostDetail;
