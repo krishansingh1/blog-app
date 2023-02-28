4ee6ec2cc6058ad5bc7fdb201dc380a1fb1b7e55
Krishan Kumar <kkrishan7074@gmail.com>
2023-02-28 23:11:48 +0530

    Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { db } from '../firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postsRef = collection(db, 'content');
+
+    onSnapshot(postsRef, (snapshot) => {
+      const posts = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      // console.log('posts', posts);
+      setPosts(posts);
+    });
+  }, []);
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Krishan</div>
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subtitle}</p>
+        </div>
+      ))}
     </div>
   );
 }
